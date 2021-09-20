a86c88d99673d71489428e925d97c207b084633e
dsy <dongshuaiyang@calmore.com>
2021-09-20 17:44:17 +0800

    完善

--- example/paddle_mnist.py
+++ example/paddle_mnist.py
@@ -39,37 +39,25 @@ class MyDataset(paddle.io.Dataset):
         return img, np.array([label]).astype('int64')
 
 
-# 定义 LeNet 网络结构
 class LeNet(paddle.nn.Layer):
     def __init__(self, num_classes=1):
         super(LeNet, self).__init__()
-        # 创建卷积和池化层
-        # 创建第1个卷积层
         self.conv1 = Conv2D(in_channels=1, out_channels=6, kernel_size=5)
         self.max_pool1 = MaxPool2D(kernel_size=2, stride=2)
-        # 尺寸的逻辑：池化层未改变通道数；当前通道数为6
-        # 创建第2个卷积层
         self.conv2 = Conv2D(in_channels=6, out_channels=16, kernel_size=5)
         self.max_pool2 = MaxPool2D(kernel_size=2, stride=2)
-        # 创建第3个卷积层
         self.conv3 = Conv2D(in_channels=16, out_channels=120, kernel_size=4)
-        # 尺寸的逻辑：输入层将数据拉平[B,C,H,W] -> [B,C*H*W]
-        # 输入size是[28,28]，经过三次卷积和两次池化之后，C*H*W等于120
         self.fc1 = Linear(in_features=120, out_features=64)
-        # 创建全连接层，第一个全连接层的输出神经元个数为64， 第二个全连接层输出神经元个数为分类标签的类别数
         self.fc2 = Linear(in_features=64, out_features=num_classes)
 
-    # 网络的前向计算过程
     def forward(self, x):
         x = self.conv1(x)
-        # 每个卷积层使用Sigmoid激活函数，后面跟着一个2x2的池化
         x = F.sigmoid(x)
         x = self.max_pool1(x)
         x = F.sigmoid(x)
         x = self.conv2(x)
         x = self.max_pool2(x)
         x = self.conv3(x)
-        # 尺寸的逻辑：输入层将数据拉平[B,C,H,W] -> [B,C*H*W]
         x = paddle.reshape(x, [x.shape[0], -1])
         x = self.fc1(x)
         x = F.sigmoid(x)
@@ -77,9 +65,7 @@ class LeNet(paddle.nn.Layer):
         return x
 
 
-# 定义训练过程
 def train(model, opt, train_loader, valid_loader, epoch_num):
-    # 开启0号GPU训练
     use_gpu = False
     paddle.set_device('gpu:0') if use_gpu else paddle.set_device('cpu')
     print('start training ... ')
@@ -88,9 +74,7 @@ def train(model, opt, train_loader, valid_loader, epoch_num):
         for batch_id, data in enumerate(train_loader()):
             img = data[0]
             label = data[1]
-            # 计算模型输出
             logits = model(img)
-            # 计算损失函数
             loss_func = paddle.nn.CrossEntropyLoss(reduction='none')
             loss = loss_func(logits, label)
             avg_loss = paddle.mean(loss)
@@ -107,10 +91,8 @@ def train(model, opt, train_loader, valid_loader, epoch_num):
         for batch_id, data in enumerate(valid_loader()):
             img = data[0]
             label = data[1]
-            # 计算模型输出
             logits = model(img)
             pred = F.softmax(logits)
-            # 计算损失函数
             loss_func = paddle.nn.CrossEntropyLoss(reduction='none')
             loss = loss_func(logits, label)
             acc = paddle.metric.accuracy(pred, label)
@@ -119,15 +101,13 @@ def train(model, opt, train_loader, valid_loader, epoch_num):
         print("[validation] accuracy/loss: {:.4f}/{:.4f}".format(np.mean(accuracies), np.mean(losses)))
         model.train()
 
-    # 保存模型参数
     paddle.save(model.state_dict(), 'mnist.pdparams')
 
 
 def main():
-    # 创建模型
     model = LeNet(num_classes=10)
     # # 设置迭代轮数
-    EPOCH_NUM = 10
+    EPOCH_NUM = 20
 
     if os.path.exists("mnist.pdparams"):
 
@@ -143,7 +123,7 @@ def main():
         train(model, opt, train_loader, valid_loader, EPOCH_NUM)
 
     model.eval()
-    img = load_and_preprocess("/home/dong/tmp/no/5/2021-09-20_17-04_4.png")
+    img = load_and_preprocess("/home/dong/tmp/2021-09-14_18-09.png")
     # cv2.imshow("", img)
     # cv2.waitKey(0)
 
