3b1e876462cb36417733829176ba7b7e001fde6b
dsy <dongshuaiyang@calmore.com>
2021-10-05 02:28:44 +0800

    完善

--- test/test_hough.py
+++ test/test_hough.py
@@ -1,13 +1,11 @@
 import cv2
 import numpy as np
+import pathlib
 
 
 def degree(theta):
     ret = theta / np.pi * 180
-
-    if ret > 90:
-        return ret - 90
-    return round(ret, 0)
+    return min(abs(90 - ret), ret)
 
 
 def rotate(image, angle, center=None, scale=1.0):
@@ -22,28 +20,51 @@ def rotate(image, angle, center=None, scale=1.0):
     return rotated
 
 
-def main():
-    img = cv2.imread("/home/dong/tmp/SHENJIANG_F_00028.pdf/0.jpg", cv2.IMREAD_GRAYSCALE)
-
+def img_degree(img):
     edges = cv2.Canny(img, 50, 150, apertureSize=3)
+    lines = cv2.HoughLines(edges, 1, np.pi / 180, 800)
 
-    lines = cv2.HoughLines(edges, 1, np.pi / 180, 360)
+    if lines is None: return 0
+
+    degree_list = []
     for line in lines:
         rho, theta = line[0]
-        a = np.cos(theta)
-        b = np.sin(theta)
-        x0 = a * rho
-        y0 = b * rho
-        x1 = int(x0 + 1000 * (-b))
-        y1 = int(y0 + 1000 * (a))
-        x2 = int(x0 - 1000 * (-b))
-        y2 = int(y0 - 1000 * (a))
-        cv2.line(img, (x1, y1), (x2, y2), (0, 0, 255), 2)
-        print(f'{rho=},{theta=}', {degree(theta)})
+        degree_list.append(degree(theta))
+    return np.min(degree_list)
 
-    #img = rotate(img, 2)
-    cv2.imshow("", img)
-    cv2.waitKey(0)
+
+def main():
+
+    for path in pathlib.Path("/home/dong/tmp/zuowen/img/0").glob("**/*.jpg"):
+        print("*"*100)
+        img = cv2.imread(str(path))
+
+        edges = cv2.Canny(img, 50, 150, apertureSize=3)
+
+        lines = cv2.HoughLines(edges, 1, np.pi / 180, 800)
+
+        if lines is not None:
+            for line in lines:
+                rho, theta = line[0]
+                a = np.cos(theta)
+                b = np.sin(theta)
+                x0 = a * rho
+                y0 = b * rho
+                x1 = int(x0 + 1000 * (-b))
+                y1 = int(y0 + 1000 * (a))
+                x2 = int(x0 - 1000 * (-b))
+                y2 = int(y0 - 1000 * (a))
+                cv2.line(img, (x1, y1), (x2, y2), (0, 0, 255), 1)
+                print(f'{rho=},{theta=}', {degree(theta)})
+
+        degree_xx = img_degree(img)
+
+        img = rotate(img, degree_xx)
+        img = cv2.resize(img, (img.shape[1]//4, img.shape[0]//4))
+        img = cv2.putText(img, str(degree_xx), (0, 50), cv2.FONT_HERSHEY_COMPLEX, 1, (0, 255, 0))
+        cv2.imshow("", img)
+        cv2.waitKey(0)
+        cv2.imwrite("/home/dong/tmp/tmp.jpg", img)
 
 
 if __name__ == "__main__":
--- test/tutorial_py_contours_begin.py
+++ test/tutorial_py_contours_begin.py
@@ -2,7 +2,7 @@ import cv2
 import numpy as np
 
 
-img_path = '/home/dong/tmp/2021-10-01_10-19.png'
+img_path = '/home/dong/tmp/zuowen/img/0/JUYE_F_00018.pdf-9.jpg'
 
 img = cv2.imread(img_path, cv2.IMREAD_GRAYSCALE)
 #img = ~img[10: -10, 10: -10]
@@ -10,6 +10,8 @@ img = cv2.imread(img_path, cv2.IMREAD_GRAYSCALE)
 _, img = cv2.threshold(img, 127, 255, 0)
 contours, hierarchy = cv2.findContours(img, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
 contours = sorted(contours, key=cv2.contourArea, reverse=True)
+
+print(contours[3])
 
 x, y, w, h = cv2.boundingRect(contours[3])
 
