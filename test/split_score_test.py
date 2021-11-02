3821ebb13b8ae013294dafe44163166156a3e14e
dsy <dongshuaiyang@calmore.com>
2021-11-02 14:09:08 +0800

    切割等分卡表格

--- test/split_score_test.py
+++ test/split_score_test.py
@@ -1,34 +1,74 @@
 import cv2
-import pathlib
-import os
+import numpy as np
 
 
-def split(img_path, output, mode = 1):
+def sort_contours(cnts, method="left-to-right"):
+    # initialize the reverse flag and sort index
+    reverse = False
+    i = 0
 
-    *_, filename = img_path.split("/")
-    img = cv2.imread(img_path)
+    # handle if we need to sort in reverse
+    if method == "right-to-left" or method == "bottom-to-top":
+        reverse = True
 
-    os.makedirs(output, exist_ok = True)
+    # handle if we are sorting against the y-coordinate rather than
+    # the x-coordinate of the bounding box
+    if method == "top-to-bottom" or method == "bottom-to-top":
+        i = 1
 
-    h = 106
+    # construct the list of bounding boxes and sort them from top to
+    # bottom
+    boundingBoxes = [cv2.boundingRect(c) for c in cnts]
+    (cnts, boundingBoxes) = zip(*sorted(zip(cnts, boundingBoxes),
+                                        key=lambda b: b[1][i], reverse=reverse))
 
-    if mode == 1:
-        columns = [(994, 1160), (1160, 1316), (1472, 1644), (1644,1800), (1942, 2108), (2108, 2270)]
-        first_row = 670
-        row_cnt = 24
-    else:
-        columns = [(994, 1160), (1160, 1316), (1472, 1644), (1644,1800), (1942, 2108), (2108, 2270)]
-        first_row = 408
-        row_cnt = 25
-
-    for i in range(row_cnt):
-        row = first_row + i * h
-        for j, column in enumerate(columns):
-            cv2.imwrite(f"{output}/{filename}-{i}-{j}.jpg", img[row: row + h, column[0]: column[1]])
+    # return the list of sorted contours and bounding boxes
+    return (cnts, boundingBoxes)
 
 
-for f in pathlib.Path("/home/dong/tmp/score/img/0").iterdir():
-    split(str(f), "/home/dong/tmp/dataset/voc/no", 0)
+img_raw = cv2.imread("/home/dong/tmp/score/img/0/SHENJIANG_F_00028.pdf-3.jpg")
+img_gray = cv2.cvtColor(img_raw, cv2.COLOR_BGR2GRAY)
 
-for f in pathlib.Path("/home/dong/tmp/score/img/1").iterdir():
-    split(str(f), "/home/dong/tmp/dataset/voc/no", 1)
+thresh, img_bin = cv2.threshold(img_gray, 0, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
+img_bin = 255-img_bin
+
+kernel_length = np.array(img_bin).shape[1]//40
+verticle_kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (1, kernel_length))
+hori_kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (kernel_length, 1))
+
+verticle_lines_img = cv2.morphologyEx(img_bin, cv2.MORPH_OPEN, verticle_kernel, iterations=3)
+verticle_lines_img = cv2.morphologyEx(verticle_lines_img, cv2.MORPH_CLOSE, verticle_kernel, iterations=100)
+
+horizontal_lines_img = cv2.morphologyEx(img_bin, cv2.MORPH_OPEN, hori_kernel, iterations=3)
+horizontal_lines_img = cv2.morphologyEx(horizontal_lines_img, cv2.MORPH_CLOSE, hori_kernel, iterations=100)
+
+
+alpha = 0.5
+beta = 1.0 - alpha
+
+img_final_bin = cv2.addWeighted(verticle_lines_img, alpha, horizontal_lines_img, beta, 0.0)
+
+(thresh, img_final_bin) = cv2.threshold(img_final_bin, 128, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
+
+contours, hierarchy = cv2.findContours(
+        img_final_bin, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+
+(contours, boundingBoxes) = sort_contours(contours, method="top-to-bottom")
+
+
+idx = 0
+for c in contours:
+    # Returns the location and width,height for every contour
+    x, y, w, h = cv2.boundingRect(c)
+
+    # If the box height is greater then 20, widht is >80, then only save it as a box in "cropped/" folder.
+    if 80 < w < 170 and 80 < h < 170:
+        idx += 1
+        new_img = img_raw[y:y + h, x:x + w]
+        cv2.imwrite("output/" + str(idx) + '.png', new_img)
+
+cv2.imwrite("t.jpg", img_final_bin)
+cv2.drawContours(img_raw, contours, -1, (0, 0, 255), 3)
+cv2.imwrite("t1.jpg", img_raw)
+cv2.imwrite("verticle_lines_img.jpg", verticle_lines_img)
+cv2.imwrite("horizontal_lines_img.jpg", horizontal_lines_img)
